d6d5981ea2784b52cd86b8cd033e4ba8021410cb
uyarahmet <85699024+uyarahmet@users.noreply.github.com>
2021-06-24 10:00:46 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -340,51 +340,6 @@ function App() {
      		</div>
      	</div>
 
-     	<div class="row contact-form">
-
-     		<div class="col-twelve">
-
-
-              <form name="contactForm" id="contactForm" method="post" action="">
-        			<fieldset>
-
-                    <div class="form-field">
-   						   <input name="contactName" type="text" id="contactName" placeholder="Name" value="" minlength="2" required=""/>
-                    </div>
-                    <div class="form-field">
-  	      			   <input name="contactEmail" type="email" id="contactEmail" placeholder="Email" value="" required=""/>
-  	               </div>
-                    <div class="form-field">
-  	     				   <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value=""/>
-  	               </div>
-                    <div class="form-field">
-  	                 	<textarea name="contactMessage" id="contactMessage" placeholder="message" rows="10" cols="50" required=""></textarea>
-  	               </div>
-                   <div class="form-field">
-                       <button class="submitform">Submit</button>
-                       <div id="submit-loader">
-                          <div class="text-loader">Sending...</div>
-         				      <div class="s-loader">
-  								  	<div class="bounce1"></div>
-  								  	<div class="bounce2"></div>
-  								  	<div class="bounce3"></div>
-  								</div>
-  							</div>
-                    </div>
-
-        			</fieldset>
-        		</form>
-
-              <div id="message-warning">
-              </div>
-        		<div id="message-success">
-                 <i class="fa fa-check"></i>Your message was sent, thank you! <br/>
-        		</div>
-
-           </div>
-
-     	</div>
-
      	<div class="row contact-info">
 
      		<div class="col-four tab-full">
